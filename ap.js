216c236380a9f2371be260ce966af7b55a4e774c
Nyapata <158186404+Nyapata@users.noreply.github.com>
2024-01-31 03:33:12 +0530

    Create ap.js

---
+++ ap.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+// Function to make the HTTP request
+const makeHttpRequest = async () => {
+  try {
+    const respons = await axios.get('https://backendworking.onrender.com/');
+    console.log('Response:', respons.data, Date.now());
+  } catch (error) {
+    console.error('Error making HTTP request:', error.message);
+  }
+};
+
+// Set up the interval to make the HTTP request every 10 seconds (10000 milliseconds)
+const intervalId = setInterval(makeHttpRequest, 60000);
+
+// Express route for a simple test endpoint
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+// Optionally, you can stop the interval after a certain number of repetitions or based on some condition
+// For example, stop after 5 minutes (30 repetitions)
+// setTimeout(() => clearInterval(intervalId), 5 * 60 * 1000);
